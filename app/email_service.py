04223d613dd763a74c26663ba71d6fae53a7e852
abbyquarm <143033707+abbyquarm@users.noreply.github.com>
2023-11-01 17:33:22 -0400

    Ask user to input email address

--- app/email_service.py
+++ app/email_service.py
@@ -11,10 +11,10 @@ load_dotenv() # go look in the .env file for any env vars
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDER_ADDRESS = os.getenv("SENDER_ADDRESS")
 
+user_address = input("Please enter the receipient email address:")
 
 
-
-def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Testing 123", html_content="<p>Hello World</p>"):
+def send_email(recipient_address= user_address, subject="[Shopping Cart App] Testing 123", html_content="<p>Hello World</p>"):
     print("SENDING EMAIL TO:", recipient_address)
     print("SUBJECT:", subject)
     print("HTML:", html_content)
