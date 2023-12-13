0fcc9d03ae75d35220f1eac8c3866cc04d84c0eb
abbyquarm <143033707+abbyquarm@users.noreply.github.com>
2023-12-13 15:24:30 -0500

    render hTML files

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,8 +8,16 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
-    #return render_template("home.html")
+    #return "Welcome Home"
+    return render_template("home.html")
+
+
+@home_routes.route("/about")
+def about():
+    print("ABOUT...")
+    return render_template("about.html")
+
+
 
 @home_routes.route("/api/books.json")
 def books():
@@ -43,6 +51,9 @@ def hello_world():
     name = url_params.get("name") or "World"
 
     message = f"Hello, {name}!"
-    return message
+    #return message
+    return render_template("hello.html", message=message)
+
+
 
     
